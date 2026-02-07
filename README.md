1844917a4e4e8ec3b664e783cf0b3e08af5c39a9
Mert Kurttutan <kurttutan.mert@gmail.com>
2026-02-07 14:53:06 +0300

    Update codex and add boot update instructions to readme

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ a version controlled system for my local tools
 The intended workflow is to update here (controlled with git). Then sync my actual nixos folder with the repo
 then run 
 `nixos-rebuild switch`
+
+To change the default boot selection for the next reboot, you can use the following command:
+`sudo /run/current-system/bin/switch-to-configuration boot`
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     claude-code.url = "github:sadjow/claude-code-nix/08c857d3f5ecbf16b8de8c7d6b83d277a064a406";
-    codex.url = "github:sadjow/codex-cli-nix/33eee8358574bd0358ccfafed4ffee1aa32d4b6e";
+    codex.url = "github:sadjow/codex-cli-nix/d3abf748ac6a06016a6fff14b5a99dedacf72378";
     zed.url = "github:mert-kurttutan/zed-nix";
     typst.url = "github:typst/typst-flake";
     typst.inputs.nixpkgs.follows = "nixpkgs";
