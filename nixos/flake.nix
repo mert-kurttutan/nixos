464344b162ee33947e8fb901239e8515c803db3a
mert-kurttutan <kurttutan.mert@gmail.com>
2026-09-09 18:03:34 +0300

    Prefer NixOS tarball and zstd compression

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     # home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
